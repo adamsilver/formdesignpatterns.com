b38846fff76bc3674d31ac59b3d87bae70a121ee
Adam Silver <adambsilver@gmail.com>
2018-09-02 09:27:03 +0100

    Added gulp and sass

---
+++ gulp-tasks/watch.js
@@ -0,0 +1,10 @@
+var project = require('./_project.js');
+var gulp    = require('gulp');
+
+/*
+  Watch folders for changess
+*/
+gulp.task("watch", function () {
+  gulp.watch(project.buildSrc + "/scss/**/*", gulp.parallel('styles'));
+  gulp.watch(project.buildSrc + "/site/**/*",  gulp.parallel('generate'));
+});
