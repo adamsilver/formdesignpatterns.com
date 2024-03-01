b6edce503203de60c4ba66f0ccdb839f937398d0
Adam Silver <adambsilver@gmail.com>
2024-03-01 08:19:29 +0000

    Upgrade - remove gulp and update CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,25 @@
 module.exports = eleventyConfig => {
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
   eleventyConfig.addFilter('permalink', str => {
     return str.replace(/\.html/g, '')
-  })
+  });
 
   eleventyConfig
     .addPassthroughCopy('src/assets/fonts/')
